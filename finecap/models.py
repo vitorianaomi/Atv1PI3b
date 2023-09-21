ffdaf7daf83f522944c83953a430fbffd8c0e434
VITÓRIA NAOMÍ MARTINS REIS <vitoria.naomi@academico.ifrn.edu.br>
2023-09-21 06:55:02 -0300

    Atividade

--- finecap/models.py
+++ finecap/models.py
@@ -16,7 +16,7 @@ class Reserva(models.Model):
     quitado = models.BooleanField()
     img = models.ImageField(upload_to = 'imagens', verbose_name = 'imagem')
     stand = models.ForeignKey(Stand, on_delete = models.CASCADE)
-    data = models.DateField()
+    data= models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.cnpj
--- finecap/views.py
+++ finecap/views.py
@@ -3,15 +3,19 @@ from .models import Reserva
 from .forms import ReservaForm
 
 # Create your views here.
+
+
 def index(request):
     objetos = Reserva.objects.all()
-    context = {'objetos' : objetos}
+    context = {'objetos': objetos}
     return render(request, 'index.html', context)
+
 
 def reserva_remover(request, id):
     reserva = get_object_or_404(Reserva, id=id)
     reserva.delete()
-    return redirect('reserva_listar') 
+    return redirect('reserva_listar')
+
 
 def reserva_criar(request):
     if request.method == 'POST':
@@ -25,12 +29,25 @@ def reserva_criar(request):
         form = ReservaForm()
     return render(request, 'formreserva.html', {'form': form})
 
+
 def reserva_listar(request):
     reservas = Reserva.objects.all().order_by('data')
-    context = {'reservas' : reservas}
-    return render(request, 'reservas.html', context)
+    if (request.GET.get('nome_empresa')):
+        reservas = reservas.filter(
+            nome_empresa__contains=request.GET.get('nome_empresa'))
+    if (request.GET.get('quitado')):
+        reservas = reservas.filter(quitado=True)
+    if (request.GET.get('valor')):
+        reservas = reservas.filter(stand__valor=request.GET.get('valor'))
+    if (request.GET.get('data')):
+        reservas = reservas.filter(data__date=request.GET.get('data'))
+    context = {
+        'reservas': reservas
+    }
+    return render(request, "reservas.html", context)
+
 
 def reserva_detalhar(request, id):
     reserva = get_object_or_404(Reserva, id=id)
-    context={'reserva' : reserva}
+    context = {'reserva': reserva}
     return render(request, 'detalhe.html', context)
